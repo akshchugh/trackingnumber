15b304555c6744a5496bdb724ba84c1d374cbc59
Aakash Chugh <akshchugh.ac@gmail.com>
2015-07-28 09:50:50 +0530

    Adding test cases for insertion

---
+++ src/TrackingTableTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrackingTableTest {
+
+	@Test
+	public void insertTest() {
+		TrackingTable objectfortesting = new TrackingTable();
+		objectfortesting.insert(new TrackingRow(1, 10000, 1, 1));
+		for(TrackingRow tr:objectfortesting.table) {
+			if(tr.notDeleted) 
+				assertEquals("Correct",tr.r.getHi(),10000);	
+		}
+		
+	}
+	
+}
