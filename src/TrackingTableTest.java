d5005f8243dcaf348c4ff3810e67f3554225287f
Aakash Chugh <akshchugh.ac@gmail.com>
2015-07-28 10:52:06 +0530

    Adding testcases for merging

--- src/TrackingTableTest.java
+++ src/TrackingTableTest.java
@@ -8,22 +8,41 @@ public class TrackingTableTest {
 	public void insertTest() {
 		TrackingTable objectfortesting = new TrackingTable();
 		objectfortesting.insert(new TrackingRow(1, 10000, 1, 1));
-		for (TrackingRow tr : objectfortesting.table) {
-			if (tr.notDeleted)
-				assertEquals("Correct", tr.r.getHi(), 10000);
+		for(TrackingRow tr:objectfortesting.table) {
+			if(tr.notDeleted) 
+				assertEquals("Correct",tr.r.getHi(),10000);	
 		}
 	}
-
 	@Test
 	public void insertTest1() {
 		TrackingTable objectfortesting = new TrackingTable();
 		objectfortesting.insert(new TrackingRow(1, 10000, 1, 1));
-		for (TrackingRow tr : objectfortesting.table) {
-			if (tr.notDeleted) {
-				assertEquals("Correct", tr.r.getLo(), 1);
+		for(TrackingRow tr:objectfortesting.table) {
+			if(tr.notDeleted) {
+				assertEquals("Correct",tr.r.getLo(),1);	
 			}
 		}
-
+	}
+	int lo;
+	int hi;
+	@Test
+	public void insertTest2includingmerging() {
+		int loopruntimes = 0;
+		TrackingTable objectfortesting = new TrackingTable();
+		objectfortesting.insert(new TrackingRow(1, 10000, 1, 1));
+		objectfortesting.insert(new TrackingRow(1, 500, 1, 2));
+		for(TrackingRow tr:objectfortesting.table) {
+			if(tr.notDeleted) {
+				hi=tr.r.getHi();
+				lo=tr.r.getLo();
+				loopruntimes++;
+				
+			}
+		}
+		assertEquals("Correct",loopruntimes,2);		
+		assertEquals("Correct",hi,10000);
+		assertEquals("Correct",lo,501);
+		
 	}
 
 }
